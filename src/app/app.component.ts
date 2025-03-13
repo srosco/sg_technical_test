453c8f08184dc1702e23c5404adbcacb88c22b0a
Steve ROSCO <srosco@consultants-solutec.fr>
2025-03-13 16:01:12 +0100

    feat(sg-test): implementing a technical test that has been done this morning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,45 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { map, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'sg';
+  mockedData$: Observable<any> = of({
+    results: [
+      {
+        "id": "1",
+        "name": "Blue Ranger",
+        "about": "The first one.",
+        "address": "1st avenue, paris"
+      },
+      {
+        "id": "2",
+        "name": "Yellow Ranger",
+        "about": "The second one.",
+        "address": "3rd avenue, Montreal"
+      },
+      {
+        "id": "3",
+        "name": "Green Ranger",
+        "about": "The third one.",
+        "address": "10th avenue, Florida"
+      },
+      {
+        "id": "4",
+        "name": "Red Ranger",
+        "about": "The fourth one.",
+        "address": "18 street, Paris"
+      }
+    ]
+  }).pipe(
+    map((data) => {
+      return data.results
+    })
+  );
+  
 }
